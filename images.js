61729cafcfcaf9fc4b3ed8c59e5b8040614c2f09
Silvia <silcasavola@gmail.com>
2024-06-25 17:54:12 +0200

    yu

--- images.js
+++ images.js
@@ -1,11 +1,58 @@
 import * as utils from './utils.js';
 let allFramesGenerated = false;
-const maxRetries = 3;
 let firstImageHeightVh;
 let metadataArray = [];
 
+// Function to set the width of title and metadata rows
+function setElementWidths(imgContainer) {
+    const width = imgContainer.clientWidth;
+    const frame = imgContainer.closest('.frame');
+    if (frame) {
+        const titleRow = frame.querySelector('.title-row');
+        const metadataLayoutElement = frame.querySelector('.metadata-layout');
+        if (titleRow) titleRow.style.width = `${width}px`;
+        if (metadataLayoutElement) metadataLayoutElement.style.width = `${width}px`;
+    }
+}
+
 // Function to create each frame element
-function createFrameElement(title, url, idx, people, place) {
+function createFrameElement(title, url, idx, people, place, isLast) {
+    let frameElement = utils.createElement('div', 'frame');
+
+    let imgContainer = utils.createElement('div', 'img-container');
+    if (isLast) {
+        imgContainer.classList.add('last');
+    }
+    let imageElement = document.createElement('img');
+    imageElement.src = url;
+    imageElement.className = `image-idx-${idx}`; // Assign class based on idx
+
+    imageElement.onload = () => {
+        setElementWidths(imgContainer);
+    };
+
+    imgContainer.append(imageElement);
+
+    let titleRow = utils.createElement('div', 'title-row', title);
+    let metadataLayoutElement = utils.createElement('div', 'metadata-layout');
+
+    if (place) {
+        metadataLayoutElement.append(
+            utils.createElement('span', 'place metadata-row', `Taken in ${place} `)
+        );
+    }
+    if (people) {
+        metadataLayoutElement.append(
+            utils.createElement('span', 'people metadata-row', `with ${people}`)
+        );
+    }
+
+    frameElement.append(imgContainer, titleRow, metadataLayoutElement);
+    return frameElement;
+}
+
+// Function to create the first image frame element and set the height
+function createFirstImageFrame(title, url, idx, people, place) {
     let frameElement = utils.createElement('div', 'frame');
 
     let imgContainer = utils.createElement('div', 'img-container');
@@ -17,13 +64,12 @@ function createFrameElement(title, url, idx, people, place) {
             firstImageHeightVh = imageElement.clientHeight / window.innerHeight * 100;
             setImageHeights(firstImageHeightVh);
         }
+        setElementWidths(imgContainer);
     };
     imgContainer.append(imageElement);
     imgContainer.append(utils.createElement('div', 'sfumatura-verticale'));
 
-
     let titleRow = utils.createElement('div', 'title-row', title);
-
     let metadataLayoutElement = utils.createElement('div', 'metadata-layout');
 
     if (place) {
@@ -52,6 +98,8 @@ export async function loadAndDisplayImages(records, metadataMain, parentId) {
         let frameContainer = utils.createElement('div', 'frame-container');
         let outerFrame = utils.createElement('div', 'outer-frame');
 
+        outerFrame.append(utils.createElement('div', 'sfumatura-verticale'));
+
         let dotspan = utils.createElement('span', 'dot');
 
         frameContainer.append(outerFrame, dotspan);
@@ -59,10 +107,41 @@ export async function loadAndDisplayImages(records, metadataMain, parentId) {
         let shotIndexes = utils.parseShotIndex(record.shot);
         let ids = utils.getIdsFromShots(shotIndexes);
 
-        for (let i = 0; i < ids.length; i++) {
+        if (ids.length > 0) {
+            let id = ids[0];
+            let currentMetadata = utils.getMetadataFromId(metadataMain, id);
+
+            if (currentMetadata) {
+                let firstFrameElement = createFirstImageFrame(
+                    currentMetadata.Title,
+                    `https://gradim.fh-potsdam.de/omeka-s/files/large/${id}.jpg`,
+                    0,
+                    currentMetadata.metaDepictedPeople,
+                    record.place
+                );
+
+                outerFrame.appendChild(firstFrameElement);
+
+                metadataArray.push({
+                    element: firstFrameElement,
+                    outerFrame: outerFrame,
+                    photonumElement: null, // Placeholder, will be updated later if needed
+                    arrowleftElement: null,
+                    arrowrightElement: null,
+                    metadata: {
+                        title: currentMetadata.Title,
+                        idx: 0, // Storing 0-based index
+                        length: ids.length,
+                        recordIndex: recordIndex // Store the record index for debugging
+                    }
+                });
+            }
+        }
+
+        for (let i = 1; i < ids.length; i++) {
             let id = ids[i];
             let currentMetadata = utils.getMetadataFromId(metadataMain, id);
-            // console.log(`Processing image id: ${id}, index: ${i}, recordIndex: ${recordIndex}`); // Debug log
+            const isLast = i === ids.length - 1;
 
             if (currentMetadata) {
                 let frameElement = createFrameElement(
@@ -70,13 +149,12 @@ export async function loadAndDisplayImages(records, metadataMain, parentId) {
                     `https://gradim.fh-potsdam.de/omeka-s/files/large/${id}.jpg`,
                     i,
                     currentMetadata.metaDepictedPeople,
-                    record.place
+                    record.place,
+                    isLast
                 );
 
-                // Append the frame element
                 outerFrame.appendChild(frameElement);
 
-                // Store metadata in an array for easy access during scroll
                 metadataArray.push({
                     element: frameElement,
                     outerFrame: outerFrame,
@@ -90,8 +168,6 @@ export async function loadAndDisplayImages(records, metadataMain, parentId) {
                         recordIndex: recordIndex // Store the record index for debugging
                     }
                 });
-            // } else {
-            //     console.error(`Metadata not found for image id: ${id} in record ${recordIndex}`);
             }
         }
 
@@ -102,7 +178,6 @@ export async function loadAndDisplayImages(records, metadataMain, parentId) {
             let photonum = utils.createElement('span', 'photonum', `(1/${ids.length})`);
             scrollHandles.append(arrowleft, photonum, arrowright);
             frameContainer.append(scrollHandles);
-            // DA SISTEMARE!!!!!!!!!!!!!!!
 
             frameContainer.append(utils.createElement('div', 'sfumatura-sinistra'));
             frameContainer.append(utils.createElement('div', 'sfumatura-destra'));
@@ -120,9 +195,6 @@ export async function loadAndDisplayImages(records, metadataMain, parentId) {
             });
         }
 
-
-        // frameContainer.append(utils.createElement('div', 'sfumatura-verticale'));
-
         fragment.appendChild(frameContainer);
 
         // Ensure .disconnected frames are also processed correctly
@@ -136,7 +208,7 @@ export async function loadAndDisplayImages(records, metadataMain, parentId) {
             if (recordIndex === 0) {
                 frameContainer.classList.add('active');
                 dotspan.classList.add('active');
-              }
+            }
         }
     }
 
@@ -160,7 +232,6 @@ export async function loadAndDisplayImages(records, metadataMain, parentId) {
 function updateMetadata(photonumElement, idx, length) {
     if (photonumElement) {
         photonumElement.textContent = `(${idx + 1}/${length})`; // Displaying 1-based index
-        // console.log(`Updated photonum: (${idx + 1}/${length})`);
     }
 }
 
@@ -171,19 +242,29 @@ function updateInformationOnScroll(outerFrame) {
     let frameElements = outerFrame.querySelectorAll('.frame');
 
     let visibleFrameIdx = -1;
+    let visibleFrameMiddleIdx = -1;
+    let minDistance = Infinity;
+
     for (let i = 0; i < frameElements.length; i++) {
         let frameElement = frameElements[i];
         let rect = frameElement.getBoundingClientRect();
-        let frameLeft = rect.left + scrollLeft -1;
+        let frameLeft = rect.left + scrollLeft - outerFrame.offsetLeft;
 
         // Check if frame is within view
-        if (frameLeft >= scrollLeft && frameLeft < scrollLeft + outerFrameWidth) {
+        if (frameLeft < scrollLeft + outerFrameWidth && frameLeft + rect.width > scrollLeft) {
             visibleFrameIdx = i;
+            // Find the frame closest to the middle of the view
+            let frameMiddle = frameLeft + rect.width / 2;
+            let distance = Math.abs(frameMiddle - (scrollLeft + outerFrameWidth / 2));
+            if (distance < minDistance) {
+                minDistance = distance;
+                visibleFrameMiddleIdx = i;
+            }
         }
     }
 
-    if (visibleFrameIdx !== -1) {
-        let metadata = metadataArray.find(meta => meta.outerFrame === outerFrame && meta.metadata.idx === visibleFrameIdx);
+    if (visibleFrameMiddleIdx !== -1) {
+        let metadata = metadataArray.find(meta => meta.outerFrame === outerFrame && meta.metadata.idx === visibleFrameMiddleIdx);
         if (metadata) {
             let photonumElement = metadata.photonumElement;
             let arrowleftElement = metadata.arrowleftElement;
@@ -191,14 +272,12 @@ function updateInformationOnScroll(outerFrame) {
 
             updateMetadata(photonumElement, metadata.metadata.idx, metadata.metadata.length);
 
-            // console.log(`Scroll - Index: ${visibleFrameIdx}, Length: ${metadata.metadata.length}, RecordIndex: ${metadata.metadata.recordIndex}`); // Debug log
-
             if (arrowleftElement && arrowrightElement) {
                 arrowleftElement.style.display = (metadata.metadata.idx === 0) ? 'none' : 'inline';
                 arrowrightElement.style.display = (metadata.metadata.idx === metadata.metadata.length - 1) ? 'none' : 'inline';
             }
         } else {
-            console.error(`Metadata not found for visibleFrameIdx: ${visibleFrameIdx} in outerFrame`);
+            console.error(`Metadata not found for visibleFrameIdx: ${visibleFrameMiddleIdx} in outerFrame`);
         }
     } else {
         console.error(`No visible frame found in outerFrame`);
